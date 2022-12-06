c5d7f2fc06f769acac4ef531b99cc80578ec90a0
Spikkio55 <119514956+Spikkio55@users.noreply.github.com>
2022-12-06 14:04:22 +0100

    Create lab4.m

---
+++ lab4.m
@@ -0,0 +1,13 @@
+% 1.a
+clear
+close all
+
+% constants
+mu_S = astroConstants(4);
+mu_E= astroConstants(13);
+
+% data
+v_inf_minus = [15.1; 0; 0]; % km/s
+delta = 9200; %km 
+r_E= [1; 0; 0]; % AU
+
