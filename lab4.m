809a29b6df240bff147f438002c5349f5b5aa779
j-emauz <31942182+j-emauz@users.noreply.github.com>
2022-12-06 14:23:35 +0100

    Update lab4.m

--- lab4.m
+++ lab4.m
@@ -9,6 +9,7 @@ AU = astroConstants(2);
 
 % data
 v_inf_minus = [15.1; 0; 0]; % km/s
+v_inf = norm(v_inf_minus);
 Delta = 9200; %km 
 r_E= [1; 0; 0]*AU; % AU
 
@@ -20,3 +21,9 @@ bp = [0; -1; 0]; %behind planet
 uu = cross(Delta*up, v_inf_minus)/norm(cross(Delta*up, v_inf_minus));
 uf = cross(Delta*fp, v_inf_minus)/norm(cross(Delta*fp, v_inf_minus));
 ub = cross(Delta*bp, v_inf_minus)/norm(cross(Delta*bp, v_inf_minus));
+
+a = -mu_E/(v_inf^2);
+
+delta = 2*atan2(-a, Delta);
+e = 1/sin(delta/2);
+rp = a*(1-e);
