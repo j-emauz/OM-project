4f4c7273eb10f63a9250ca8ac0ee24dfa60879c3
Spikkio55 <119514956+Spikkio55@users.noreply.github.com>
2022-12-14 15:06:49 +0100

    plot

--- lab5.m
+++ lab5.m
@@ -13,7 +13,7 @@ J2=astroConstants(9);
 kep0 = [7571, 0.01, 87.9 *pi/180, 180 *pi/180, 180 *pi/180, 0 *pi/180];
 [car0, v] = par2car(kep0(1), kep0(2), kep0(3), kep0(4), kep0(5), kep0(6), mu_E);
 T = 2*pi*sqrt( kep0(1)^3/mu_E ); % Orbital period [1/s]
-tspan = linspace( 0, 100*T, 10000 );
+tspan = linspace( 0, 100*T, 10001 );
 options = odeset( 'RelTol', 1e-13, 'AbsTol', 1e-14 );
 [ T_Gauss, S_Gauss ] = ode45(@(t,s) eq_motion(t,s, @(t,s) acc_pert_fun(t,s,mu_E,J2,R_E), mu_E), tspan, kep0, options );
 % Analyse and plot the results
@@ -31,11 +31,30 @@ end
 
 %% plotting
 hold on
-N1 = 500;
-amean = movmean(S_Gauss(:, 1), N1);
+N = 100;
+amean = movmean(S_Gauss(:, 1), N);
 plot(T_Gauss/T, amean);
 plot(T_J2/T,a);
-
 plot(T_Gauss/T,S_Gauss(:,1));
+legend('average','Cartesian','Gauss')
 
-
+figure
+plot(T_Gauss/T,a-S_Gauss(:,1));
+%% 
+figure
+N = 500;
+emean = movmean(S_Gauss(:, 2), N);
+plot(T_Gauss/T,S_Gauss(:,2));
+hold on
+plot(T_J2/T,e);
+plot(T_Gauss/T, emean);
+legend('Gauss','Cartesian','average')
+%%
+figure
+N = 500;
+emean = movmean(S_Gauss(:, 2), N);
+plot(T_Gauss/T,S_Gauss(:,2));
+hold on
+plot(T_J2/T,e);
+plot(T_Gauss/T, emean);
+legend('Gauss','Cartesian','average')
