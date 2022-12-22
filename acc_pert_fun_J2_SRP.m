d0571d3d1d951f029e899606a4042b2ba4697dbd
kunfuxu <31942182+j-emauz@users.noreply.github.com>
2022-12-22 21:27:29 +0000

    Update acc_pert_fun_J2_SRP.m

--- acc_pert_fun_J2_SRP.m
+++ acc_pert_fun_J2_SRP.m
@@ -14,11 +14,17 @@ function acc_pert_vec = acc_pert_fun_J2_SRP( t, s, mu,J2,R)
      [r_S_E,~] = par2car(kep_E(1),kep_E(2),kep_E(3),kep_E(4),kep_E(5),kep_E(6),mu_S);
      [rr, vv] = par2car(s(1), s(2), s(3), s(4), s(5), s(6), mu_S);
 %     
-     r_dir = rr./norm(rr);
-     w_dir = cross(rr,vv)./norm(cross(rr,vv));
-     s_dir = cross(w_dir, r_dir);
+     %r_dir = rr./norm(rr);
+     %w_dir = cross(rr,vv)./norm(cross(rr,vv));
+     %s_dir = cross(w_dir, r_dir);
 
-     R_rsw2eci = [r_dir(:), w_dir(:), s_dir(:)];
+    R_1_i=[1 0 0; 0 cos(s(3)) sin(s(3)); 0 -sin(s(3)) cos(s(3))];
+    R_3_Om=[cos(s(4)) sin(s(4)) 0; -sin(s(4)) cos(s(4)) 0; 0 0 1];
+    R_3_om_th=[cos(s(5)+s(6)) sin(s(5)+s(6)) 0; -sin(s(5)+s(6)) cos(s(5)+s(6)) 0; 0 0 1];
+
+    
+
+     %R_rsw2eci = [r_dir(:), w_dir(:), s_dir(:)];
     
     tilt = deg2rad(23.45);
     R_eci2sce = [1   0   0;                %ECI -> Sun-Centered-Ecliptic
@@ -44,11 +50,7 @@ function acc_pert_vec = acc_pert_fun_J2_SRP( t, s, mu,J2,R)
     acc_pert_SRP= P_sun*(AU^2)/(norm(r_sc_Sun)^2)*Cr*AMR*10^-3;
     acc_pert_vec_SRP=-acc_pert_SRP*(r_sc_Sun./norm(r_sc_Sun));
     
-    acc_pert_vec_SRP = R_rsw2eci'*acc_pert_vec_SRP;
+    acc_pert_vec_SRP = R_3_om_th*R_1_i*R_3_Om*acc_pert_vec_SRP;
     acc_pert_vec=acc_pert_vec_J2 + acc_pert_vec_SRP;
 
-    % A_rot=h/(p*v).*[s(2)*sin(s(6)),  - (1+s(2)*cos(s(6)));
-    %                    1+s(2)*cos(s(6)),   s(2)*sin(s(6))];
-    % a_tn = A_rot\[a_rsw(1); a_rsw(2)];
-    % acc_pert_vec=[a_tn; a_rsw(3)];
 end
