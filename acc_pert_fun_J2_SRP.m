7e2c6fa64bdf999e973580b57bbf054e9845189e
kunfuxu <31942182+j-emauz@users.noreply.github.com>
2022-12-22 21:42:33 +0000

    last change

--- acc_pert_fun_J2_SRP.m
+++ acc_pert_fun_J2_SRP.m
@@ -12,8 +12,8 @@ function acc_pert_vec = acc_pert_fun_J2_SRP( t, s, mu,J2,R)
      t_days=t/3600/24; %days since initial time
      [kep_E,~]=uplanet(initial_time+t_days,3);
      [r_S_E,~] = par2car(kep_E(1),kep_E(2),kep_E(3),kep_E(4),kep_E(5),kep_E(6),mu_S);
-    % [rr, vv] = par2car(s(1), s(2), s(3), s(4), s(5), s(6), mu_S);
-%     
+     %[rr, vv] = par2car(s(1), s(2), s(3), s(4), s(5), s(6), mu);
+     
      %r_dir = rr./norm(rr);
      %w_dir = cross(rr,vv)./norm(cross(rr,vv));
      %s_dir = cross(w_dir, r_dir);
@@ -24,22 +24,14 @@ function acc_pert_vec = acc_pert_fun_J2_SRP( t, s, mu,J2,R)
 
     
 
-     %R_rsw2eci = [r_dir(:), w_dir(:), s_dir(:)];
+     %R_rsw2eci = [r_dir(:), s_dir(:), w_dir(:)];
     
     tilt = deg2rad(23.45);
     R_eci2sce = [1   0   0;                %ECI -> Sun-Centered-Ecliptic
           0  cos(tilt)   sin(tilt);
          0  -sin(tilt)  cos(tilt)];
-   %R_sce2eci1 = [1, 0, 0];
-   %R_sce2eci2 = [0 cos(tilt) -sin(tilt)];
-   %R_sce2eci3 = [0 sin(tilt) cos(tilt)];
 
     r_sc_Sun= - R_eci2sce'*(r_S_E); 
-     %r_sc_Sun1 = R_sce2eci1*(r_E_S);
-     %r_sc_Sun2 = R_sce2eci2*(r_E_S);
-     %r_sc_Sun3 = R_sce2eci3*(r_E_S);
-     %r_sc_Sun = [r_sc_Sun1; r_sc_Sun2; r_sc_Sun3]
-     %r_sc_Sun = r_E_S;
 
     p = s(1)*(1-s(2)^2);
     r = p/(1 + s(2)*cos(s(6)));
@@ -50,7 +42,8 @@ function acc_pert_vec = acc_pert_fun_J2_SRP( t, s, mu,J2,R)
     acc_pert_SRP= P_sun*(AU^2)/(norm(r_sc_Sun)^2)*Cr*AMR*10^-3;
     acc_pert_vec_SRP=-acc_pert_SRP*(r_sc_Sun./norm(r_sc_Sun));
     
-    acc_pert_vec_SRP = R_3_om_th*R_1_i*R_3_Om*acc_pert_vec_SRP;
+    %acc_pert_vec_SRP = R_rsw2eci'*acc_pert_vec_SRP;
+     acc_pert_vec_SRP = R_3_om_th*R_1_i*R_3_Om*acc_pert_vec_SRP;
     acc_pert_vec=acc_pert_vec_J2 + acc_pert_vec_SRP;
 
 end
